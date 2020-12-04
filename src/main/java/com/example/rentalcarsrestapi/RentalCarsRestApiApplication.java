cfa2fe4d0194e1c4e59d9da2a9320c569860a44e
Jaroslaw Ziecik <jaroslaw.ziecik@gmail.com>
2020-12-04 16:04:38 +0100

    RCRA-1 #done #comment Add basic dependencies and configure mysql connection

---
+++ src/main/java/com/example/rentalcarsrestapi/RentalCarsRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentalcarsrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCarsRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalCarsRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rentalcarsrestapi/RentalCarsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentalcarsrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCarsRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
