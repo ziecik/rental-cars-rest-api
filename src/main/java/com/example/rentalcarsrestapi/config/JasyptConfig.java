c9608e57c2ad21860e4f819dabd0036ef2318ddc
ziecik <jaroslaw.ziecik@gmail.com>
2021-02-11 22:39:21 +0100

    #RCRA-31 #done #comment Implement and configure jasypt

---
+++ src/main/java/com/example/rentalcarsrestapi/config/JasyptConfig.java
@@ -0,0 +1,9 @@
+package com.example.rentalcarsrestapi.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+}
